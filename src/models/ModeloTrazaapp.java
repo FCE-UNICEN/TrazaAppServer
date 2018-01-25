00a53d0f56d751a2629263c946c9e803a7bcce4c
juanfran93 <jfferreira1993@gmail.com>
2018-01-25 10:45:15 -0300

    servicios nuevos
    servicios /getresources y /addpeticion

--- src/models/ModeloTrazaapp.java
+++ src/models/ModeloTrazaapp.java
@@ -2,6 +2,7 @@ package models;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ModeloTrazaapp {
 
@@ -23,21 +24,19 @@ public class ModeloTrazaapp {
 
 	// ------- ++++++++++++++++ -------
 
-	public LoginToken attemptLogin(String email, String password){
+	public LoginToken attemptLogin(String email, String password) {
 
-		try{
-			String query = "SELECT * FROM USERS " +
-					"WHERE email='"+ email +"'" +
-					"  AND PASSWORD='"+password+"'";
+		try {
+			String query = "SELECT * FROM USERS " + "WHERE email='" + email + "'" + "  AND PASSWORD='" + password + "'";
 			System.out.println(query);
 			ResultSet resultSet = sql.getResultset(query);
 
-			if(resultSet.next()){
+			if (resultSet.next()) {
 				System.out.println("Existe el usuario");
 				// conn.close();
 				boolean admin = false;
-				System.out.println("Es admin:" +resultSet.getString("role_id"));
-				if(resultSet.getInt("role_id")==1){
+				System.out.println("Es admin:" + resultSet.getString("role_id"));
+				if (resultSet.getInt("role_id") == 1) {
 					admin = true;
 				}
 
@@ -58,110 +57,145 @@ public class ModeloTrazaapp {
 				result.setStatus(200);
 				result.setUsuario(usuario);
 				return result;
-			}
-			else{
+			} else {
 				System.out.println("usuario no-valido");
 				LoginToken result = new LoginToken();
 				result.setMensaje("Usuario y contraseña no coinciden");
 				result.setStatus(401);
 				result.setUsuario(new Persona());
 				return result;
-				
+
 			}
 		} catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
-	public boolean existeUsuario(String email){
+public ResourceToken getResouces(){
+		
+		String query = "SELECT id,name FROM RESOURCES";
+		System.out.println(query);
+		ResultSet resultSet = sql.getResultset(query);
+		ArrayList<Resource> recursos = new ArrayList<>();
+		try {
+			while(resultSet.next()){
+				Resource rec = new Resource();
+				System.out.println("--");
+				rec.setId(resultSet.getInt("id"));
+				rec.setNombre(resultSet.getString("name"));	
+				recursos.add(rec);
+				System.out.println(rec.getNombre());
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ResourceToken rt = new ResourceToken();
+		rt.setMensaje("RECURSOS CARGADOS");
+		rt.setResources(recursos.toArray(new Resource[recursos.size()]));
+		//rt.setResources((Resource[]) recursos.toArray());
+		rt.setStatus(200);
+		
+		return rt;
+		
+	}
 
-		try{
-			String query = "SELECT * FROM USERS " +
-					"WHERE email='"+ email +"'";
+	public boolean existeUsuario(String email) {
+
+		try {
+			String query = "SELECT * FROM USERS " + "WHERE email='" + email + "'";
 			System.out.println(query);
 			ResultSet resultSet = sql.getResultset(query);
 
-			if(resultSet.next()){
+			if (resultSet.next()) {
 				System.out.println("Existe el usuario");
 				return true;
-			}
-			else{
+			} else {
 				System.out.println("usuario no existe");
 				return false;
-				
+
 			}
 		} catch (SQLException e) {
-            e.printStackTrace();
-            return true;
-        }
+			e.printStackTrace();
+			return true;
+		}
 	}
-	
-	
 
-	public SimpleToken registerUser(String email, String password, String usuario, String nombre, String direccion, String telefono, int ciudad, String web, Double latitud, Double longitud ) {
+	public SimpleToken registerUser(String email, String password, String usuario, String nombre, String direccion,
+			String telefono, int ciudad, String web, Double latitud, Double longitud) {
 		// TODO Auto-generated method stub
-		if(existeUsuario(email)){
+		if (existeUsuario(email)) {
 			SimpleToken result = new SimpleToken();
 			result.setMensaje("El email ya existe");
 			result.setStatus(400);
-		//mandar token diciendo q no se pudo registrar usuario
+			// mandar token diciendo q no se pudo registrar usuario
+			return result;
+		} else {
+
+			String query = "INSERT INTO USERS(role_id,username,password,name,email,address,phone,"
+					+ "location_id,website,latitude, longitude) " + "VALUES(2,'" + usuario + "','" + password + "',"
+					+ "'" + nombre + "','" + email + "'," + "'" + direccion + "','" + telefono + "'," + "'" + ciudad
+					+ "','" + web + "'," + " " + latitud + "," + " " + longitud + ")";
+			sql.executeQuery(query);
+			SimpleToken result = new SimpleToken();
+			result.setMensaje("Usuario creado correctamente");
+			result.setStatus(200);
+
 			return result;
 		}
-		else{
-		
-		String query = "INSERT INTO USERS(role_id,username,password,name,email,address,phone," +
-				"location_id,website,latitude, longitude) " +
-				"VALUES(2,'"+usuario+"','"+password+"'," +
-				"'"+nombre+"','"+email+"'," +
-				"'"+direccion+"','"+telefono+"'," +
-				"'"+ciudad+"','"+web+"',"+
-				" "+latitud+","+" "+longitud+")";
-		sql.executeQuery(query);
-		SimpleToken result = new SimpleToken();
-		result.setMensaje("Usuario creado correctamente");
-		result.setStatus(200);
-		
-		return result;
-		}
 	}
-	
+
 	public SimpleToken addPackage(int cantidad, Integer idPadre, int idResource) {
 		String query;
-		if (idPadre != null){
-			query = "INSERT INTO PACKAGE(cantidad,id_package_padre,id_resource) " +
-					"VALUES('"+cantidad+"','"+idPadre.intValue()+"','"+idResource+"')";
-		}else {
-			query = "INSERT INTO PACKAGE(cantidad,id_resource,enUso) " +
-					"VALUES('"+cantidad+"','"+idResource+"','0')";
+		if (idPadre != null) {
+			query = "INSERT INTO PACKAGE(cantidad,id_package_padre,id_resource) " + "VALUES('" + cantidad + "','"
+					+ idPadre.intValue() + "','" + idResource + "')";
+		} else {
+			query = "INSERT INTO PACKAGE(cantidad,id_resource,enUso) " + "VALUES('" + cantidad + "','" + idResource
+					+ "','0')";
 		}
-			
+
 		sql.executeQuery(query);
 		SimpleToken result = new SimpleToken();
 		result.setMensaje("paquete creado correctamente");
 		result.setStatus(200);
-		
+
 		return result;
 
 	}
-	
+
 	public SimpleToken sendPackage(int cantidad, Integer idPadre, int idResource) {
 		// TODO Auto-generated method stub
 		String query;
-		if (idPadre != null){
-			query = "INSERT INTO PACKAGE(cantidad,id_package_padre,id_resource) " +
-					"VALUES('"+cantidad+"','"+idPadre.intValue()+"','"+idResource+"')";
-		}else {
-			query = "INSERT INTO PACKAGE(cantidad,id_resource) " +
-					"VALUES('"+cantidad+"','"+idResource+"')";
+		if (idPadre != null) {
+			query = "INSERT INTO PACKAGE(cantidad,id_package_padre,id_resource) " + "VALUES('" + cantidad + "','"
+					+ idPadre.intValue() + "','" + idResource + "')";
+		} else {
+			query = "INSERT INTO PACKAGE(cantidad,id_resource) " + "VALUES('" + cantidad + "','" + idResource + "')";
 		}
-			
+
 		sql.executeQuery(query);
 		SimpleToken result = new SimpleToken();
 		result.setMensaje("paquete enviado correctamente");
 		result.setStatus(200);
-		
+
 		return result;
 
+	}
+
+	public SimpleToken addPeticion(int idResource, int idUser, int cantidad) {
+		String query;
+		
+		query = "INSERT INTO PETICION(id_resource,id_user,cantidad) " + "VALUES('" + idResource + "','"
+				+ idUser + "','" + cantidad + "')";
+		
+		sql.executeQuery(query);
+		SimpleToken result = new SimpleToken();
+		result.setMensaje("peticion creada exitosamente");
+		result.setStatus(200);
+
+		return result;
 	}
 }
